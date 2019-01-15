3d9789203eea56573a00fcd59aa7c782ee2a9966
marikuana <43219562+marikuana@users.noreply.github.com>
2019-01-15 15:54:25 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    prefix: "f!",
+    welcomeChannel: "529006803879591958",
+    admins: [ "412604504224169996"],
+    token: process.env.KEY,
+    fEmail: process.env.fEmail,
+    fPassWord: process.env.fPassword
+}
