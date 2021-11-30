823b6c07abb7f15fa2b0e845a7fbfbd74384a68a
ibrahimFN <89642812+ibrahimFN@users.noreply.github.com>
2021-11-30 19:57:42 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    prefix: "f!",
-    welcomeChannel: "529006803879591958",
-    admins: [ "412604504224169996"],
+    prefix: "!",
+    welcomeChannel: "840148439525883945",
+    admins: [ "796339903444680765"],
     token: process.env.KEY,
     fEmail: process.env.fEmail,
     fPassWord: process.env.fPassword
